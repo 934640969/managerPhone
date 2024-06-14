be8c99de36d29ca596748674c564efb137e38c69
huangg <huangg@eetrust.com>
2024-06-14 09:57:50 +0800

    数智后台短信登录

--- src/com/eetrust/sdms/clientinterface/manager/service/impl/ConstantV.java
+++ src/com/eetrust/sdms/clientinterface/manager/service/impl/ConstantV.java
@@ -8,7 +8,9 @@ import java.util.Properties;
 
 public class ConstantV {
 
-	public static String ocsp_ip = "";
+	public static String smsUrl = "";
+	public static String accessKey = "";
+	public static String templateId = "";
 	static{
 		Properties p = new Properties();
 		InputStream in;		
@@ -16,7 +18,9 @@ public class ConstantV {
 		try {
 			p.load(in);	
 			// 获取本应用（业务应用自身）的资源编码（在信任服务资源系统中注册后取得）及身份认证服务地址
-			ocsp_ip = p.getProperty("ocsp_ip");
+			smsUrl = p.getProperty("smsUrl");
+			accessKey = p.getProperty("accessKey");
+			templateId = p.getProperty("templateId");
 			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
